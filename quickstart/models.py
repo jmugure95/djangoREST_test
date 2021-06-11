13094f7aa04a210ca8b2001a9d6c8c93360df7e4
jmugure95 <jmugure95@gmail.com>
2021-06-11 22:11:47 +0300

    refactor the models

--- quickstart/models.py
+++ quickstart/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Item(models.Model):
-    itemId = models.IntegerField()
     itemName = models.CharField(max_length=100)
-    price = models.FloatField()
     quantity = models.IntegerField()
     category = models.CharField(max_length=200)
+    charity = models.CharField(max_length=200)
 
     def __str__(self):
         return self.itemName
@@ -19,4 +18,15 @@ class CharityRegistration(models.Model):
     city = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.email
+        return self.charity_name
+
+
+class UserRegistration(models.Model):
+    email = models.EmailField()
+    charity_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
